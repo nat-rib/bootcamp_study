b6b611f94eecee9c1726bad619fa3673fc751562
nat-rib <nferraz1195@gmail.com>
2023-09-05 09:34:12 -0300

    feat: create table billboard + docker compose

---
+++ create_table_billboard.sql
@@ -0,0 +1,9 @@
+create table public."Billboard"(
+	"date" date null,
+	"rank" int4 null,
+	song varchar(300) null,
+	artist varchar(300) null,
+	"last-week" int4 null,
+	"peak-rank" int4 null,
+	"weeks-on-board" int4 null
+)
